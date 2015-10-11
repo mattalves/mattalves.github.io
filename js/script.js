e985f2d7759020cbacee5a2229564b51b5cdc120
Rafael <rafael.lima@univem.edu.br>
2015-10-11 12:28:51 -0300

    Armazenando valores e calculos iniciais

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 posicaoLinha = 0;
 posicaoColuna = 0;
 
+var numeroLinhas = 0;
+var numeroColunas = 0;
+var minor = 9999;
+var column = 0;
+
+var tableCalc;
+var arrayMinorValue = [];
+var arrayB = [];
+
 function criarMatriz(linhasMatriz, colunasMatriz) {
    
    var x = new Array(linhasMatriz);
@@ -20,8 +29,8 @@ function montarTabela() {
 	var numeroVariaveis = parseInt(document.getElementById("textBox1").value);
 	var numeroRestricoes = parseInt(document.getElementById("textBox2").value);
 	
-	var numeroLinhas = 2 + numeroRestricoes; //Cabeçalho + Função Objetivo + Número de Restrições
-	var numeroColunas = 3 + numeroVariaveis + numeroRestricoes;
+	numeroLinhas = 2 + numeroRestricoes; //Cabeçalho + Função Objetivo + Número de Restrições
+	numeroColunas = 3 + numeroVariaveis + numeroRestricoes;
 	
 	var tabelaResposta = criarMatriz(numeroLinhas, numeroColunas);
 	
@@ -42,8 +51,12 @@ function montarTabela() {
 	while(posicaoLinha != numeroLinhas) {
 		
 		tabelaResposta[posicaoLinha][posicaoColuna++] = "L" + posicaoLinha;
-		tabelaResposta[posicaoLinha][posicaoColuna++] = "F" + posicaoLinha;
 		
+		if(posicaoLinha == (numeroLinhas-1)) {
+			tabelaResposta[posicaoLinha][posicaoColuna++] = "Z";
+		} else {
+			tabelaResposta[posicaoLinha][posicaoColuna++] = "F" + posicaoLinha;
+		}
 		while(posicaoColuna != numeroColunas) {
 		
 			var nomeEntrada = "input" + posicaoLinha + posicaoColuna;
@@ -100,4 +113,77 @@ function montarTabela() {
 	//document.getElementById("myTable").tHead.innerHTML -> <tr><th></th></tr>
 	//document.getElementById("myTable").tBodies[0].innerHTML -> <tr><td></td></tr>
 
+}
+
+function getValues(){
+	var row = 0;	
+	
+	var arrayRow = numeroLinhas - 1;
+	var arrayColumn = numeroColunas- 2;
+	
+	var nomeEntrada = "";
+	
+	// CRIANDO A MATRIZ A SER CALCULADA
+	tableCalc = criarMatriz(arrayRow, arrayColumn);	
+	
+	// INSERINDO VALORES DOS INPUTS NA MATRIZ A SER CALCULADA
+	for(var indexRow = 1; indexRow < numeroLinhas; indexRow++, row++) {			
+		for(var indexCol = 2, col = 0; indexCol < numeroColunas; indexCol++, col++) {				
+			nomeEntrada = "input" + indexRow + indexCol;
+			if($("#"+nomeEntrada).val() == ""){
+				tableCalc[row][col] = 0;
+			} else {
+				tableCalc[row][col] = $("#"+nomeEntrada).val();
+			}
+		}				
+	}
+	
+	row--;
+	arrayColumn--;
+
+	// MENOR VALOR DA COLUNA Z 
+	for(var col = 0; col < arrayColumn; col++) {
+		if(tableCalc[row][col] < minor) {
+			minor = tableCalc[row][col];
+			column = col;
+		}	
+	}
+	
+	// MONTA UM VETOR COM O MENOR VALOR DA COLUNA Z
+	for(var row = 0; row < arrayRow; row++) {
+		arrayMinorValue[row] = tableCalc[row][column];
+	}
+		
+	// MONTAR VETOR DA COLUNA B
+	for(var rows = 0; rows < arrayRow; rows++) {
+		arrayB[rows] = tableCalc[rows][arrayColumn];
+	}
+	
+	simplexCalc();
+}
+
+function simplexCalc() {
+	
+	alert(tableCalc);
+	alert(arrayMinorValue);
+	alert(arrayB);
+	
+	minor = 9999;
+	var arrayDiv = [];
+	
+	for(var index = 0; index < arrayMinorValue.length; index++) {
+		if(arrayMinorValue[index] != 0) {
+			arrayDiv[index] = arrayB[index] / arrayMinorValue[index];
+		}
+	}
+	
+	for(var index = 0; index < arrayDiv.length; index++) {
+		if(arrayDiv[index] < minor) {
+			minor = arrayDiv[index];
+			column = index;
+		}		
+	}
+	alert(arrayDiv);
+	alert(minor);
+	alert(column);
 }
