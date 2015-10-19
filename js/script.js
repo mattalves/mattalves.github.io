a313212a7de8b7b140af8cea112ec0f406d5266f
Rafael <rafael.lima@univem.edu.br>
2015-10-19 18:34:08 -0200

    ajustes

--- js/script.js
+++ js/script.js
@@ -1,160 +1,174 @@
 
 var idTable = "";
 
-var numeroLinhas  =    0;
-var numeroColunas =    0;
-var minor         = 9999;
-var column        =    0;
+var nLines =    0;
+var nCols  =    0;
+var minor  = 9999;
+var column =    0;
 
-var tableCalc;
-
+var arrayInitial    = [];
+var arraySimplex    = [];
 var arrayMinorValue = [];
 var arrayB          = [];
 var arrayColumn     = [];
 var arrayRow        = [];
 var arrayBase       = [];
-
+var arrayDiv        = [];
 var nColumn = -1;
 var hLine   =  0;
 
-var calc   = true;
+var btClick= true;
 var mTable = true;
 var check  = true;
 
-var agoraVai	 = 0;
-var row     	 = 0;
-var teste    	 = 0;
-var idMatriz     = 0;
-var posicaoLinha = 0;
-var posicaoColuna= 0;
-var cont         = 0;
+var cGetValues	= 0;
+var row     	= 0;
+var cStop    	= 0;
+var idArray     = 0;
+var positionRow = 0;
+var positionCol = 0;
+var cRepeat     = 0;
+var Pivo        = 0;
 
-function criarMatriz(linhasMatriz, colunasMatriz) {
+$(document).ready(function(){
+	$("#nVar").focus();
+});
+
+function doNothing(){
+	return false;
+}
+
+function initializeArray(nLines, nCols) {
    
-   var x = new Array(linhasMatriz);
+   var array = [nLines];
    
-   for (var n = 0; n < linhasMatriz; n++) {
+   for (var index = 0; index < nLines; index++) {
        
-	   x[n] = new Array(colunasMatriz);
+	   array[index] = [nCols];
    
    }
    
-   return x;
-
+   return array;
 }
 
-function montarTabela(idTable) {
+function mountTable(idTable) {
+
+	$('#btCalc').prop("disabled",false);
+
+	var nVar		 = parseInt($('#nVar').val());
+	var nRestritions = parseInt($('#nRestritions').val());
+
+	nLines = 2 + nRestritions; //Cabeçalho + Função Objetivo + Número de Restrições
+	nCols  = 3 + nVar + nRestritions;
 	
-	var numeroVariaveis = parseInt(document.getElementById("textBox1").value);
-	var numeroRestricoes = parseInt(document.getElementById("textBox2").value);
+	arrayInitial = initializeArray(nLines, nCols);
 	
-	numeroLinhas = 2 + numeroRestricoes; //Cabeçalho + Função Objetivo + Número de Restrições
-	numeroColunas = 3 + numeroVariaveis + numeroRestricoes;
-	
-	var tabelaResposta = criarMatriz(numeroLinhas, numeroColunas);
-	
-	tabelaResposta[posicaoLinha][posicaoColuna++] = "Linha";
-	tabelaResposta[posicaoLinha][posicaoColuna++] = "Base";
-	
-	for(var n1 = 1; posicaoColuna != (numeroVariaveis + 2); n1++)
-		tabelaResposta[posicaoLinha][posicaoColuna++] = "x" + n1;
-	
-	for(var n1 = 1, n2 = posicaoColuna + numeroRestricoes; posicaoColuna != n2; n1++)
-		tabelaResposta[posicaoLinha][posicaoColuna++] = "f" + n1;
-		
-	tabelaResposta[posicaoLinha][posicaoColuna] = "b";
-	
-	posicaoColuna = 0;
-	posicaoLinha++;
-	
-	while(posicaoLinha != numeroLinhas) {
-		
-		tabelaResposta[posicaoLinha][posicaoColuna++] = "L" + posicaoLinha;
-		
-		if(posicaoLinha == (numeroLinhas-1)) {
-			tabelaResposta[posicaoLinha][posicaoColuna++] = "Z";
-		} else {
-			tabelaResposta[posicaoLinha][posicaoColuna++] = "f" + posicaoLinha;
+	arrayInitial[positionRow][positionCol++] = "Linha";
+	arrayInitial[positionRow][positionCol++] = "Base";
+
+	var control = (nVar + nRestritions) + positionCol;
+	for(var indexX = 1, indexF = 1; positionCol < control; indexX++) {
+		if(indexX <= nVar) {
+			arrayInitial[positionRow][positionCol++] = "x" + indexX;
+		} else {			
+			arrayInitial[positionRow][positionCol++] = "f" + indexF;
+			indexF++;
 		}
-		if(calc) {
-			while(posicaoColuna != numeroColunas) {
+
+	}
+	
+	arrayInitial[positionRow][positionCol] = "b";
+	
+	positionCol = 0;
+	positionRow++;
+	
+	while(positionRow != nLines) {
+		
+		arrayInitial[positionRow][positionCol++] = "L" + positionRow;
+		
+		if(positionRow == (nLines-1)) {
+			arrayInitial[positionRow][positionCol++] = "Z";
+		} else {
+			arrayInitial[positionRow][positionCol++] = "f" + positionRow;
+		}
+		if(btClick) {
+			while(positionCol != nCols) {
 			
-				var nomeEntrada = "input" + posicaoLinha + posicaoColuna;
+				var inputName = "input" + positionRow + positionCol;
 				
-				tabelaResposta[posicaoLinha][posicaoColuna++] = '<input type="number" class="span1" id="' + nomeEntrada + '">';
+				arrayInitial[positionRow][positionCol++] = '<input type="number" class="span1" id="' + inputName + '">';
 			}
 		} 
 		
-		posicaoColuna = 0;
-		posicaoLinha++;
+		positionCol = 0;
+		positionRow++;
 		
 	}
 	
-	posicaoColuna = 0;
+	positionCol = 0;
 	
-	var cabecalhoTabela = "<tr>";
+	var headTable = "<tr>";
 	
-	while(posicaoColuna != numeroColunas) {
+	while(positionCol != nCols) {
 	
-		cabecalhoTabela = cabecalhoTabela + "<th id="+idTable+nColumn+">" + tabelaResposta[0][posicaoColuna++] + "</th>";
+		headTable = headTable + "<th id="+idTable+nColumn+">" + arrayInitial[0][positionCol++] + "</th>";
 		nColumn++;
 	}
 	
-	cabecalhoTabela = cabecalhoTabela + "</tr>";
+	headTable = headTable + "</tr>";
 	
-	document.getElementById(idTable).tHead.innerHTML = cabecalhoTabela;
+	document.getElementById(idTable).tHead.innerHTML = headTable;
 	
-	posicaoColuna = 0;
-	posicaoLinha = 1;
+	positionCol = 0;
+	positionRow = 1;
 	
-	var corpoTabela = "";
+	var bodyTable = "";
 	
-	while(posicaoLinha != numeroLinhas) {
+	while(positionRow != nLines) {
 	
-		corpoTabela = corpoTabela + "<tr>";
+		bodyTable = bodyTable + "<tr>";
 		
-		while(posicaoColuna != numeroColunas) {
+		while(positionCol != nCols) {
 			
-			corpoTabela = corpoTabela + "<td id="+idTable+posicaoLinha+posicaoColuna+">" + tabelaResposta[posicaoLinha][posicaoColuna++] + "</td>";
+			bodyTable = bodyTable + "<td id="+idTable+positionRow+positionCol+">" + arrayInitial[positionRow][positionCol++] + "</td>";
 			
 		}
 		
-		corpoTabela = corpoTabela + "</tr>";
+		bodyTable = bodyTable + "</tr>";
 		
-		posicaoColuna = 0;
-		posicaoLinha++;
+		positionCol = 0;
+		positionRow++;
 		
 	}
 	
-	document.getElementById(idTable).tBodies[0].innerHTML = corpoTabela;
-	posicaoLinha  = 0;
-	posicaoColuna = 0;
+	document.getElementById(idTable).tBodies[0].innerHTML = bodyTable;
+	positionRow  = 0;
+	positionCol = 0;
 	nColumn = -1;
 }
 
 
 function getValues(){
-	calc = false;
-	
-	//alert(row);
+	btClick = false;
+		
 	// CRIANDO A MATRIZ A SER CALCULADA			
 	if(mTable){
 		row = 0;
-		arrayRow = numeroLinhas - 1;
-		arrayColumn = numeroColunas- 2;
+		arrayRow = nLines - 1;
+		arrayColumn = nCols- 2;
 		
-		var nomeEntrada = "";
+		var inputName = "";
 		
-		tableCalc = criarMatriz(arrayRow, arrayColumn);
+		arraySimplex = initializeArray(arrayRow, arrayColumn);
 		
 		// INSERINDO VALORES DOS INPUTS NA MATRIZ A SER CALCULADA
-		for(var indexRow = 1; indexRow < numeroLinhas; indexRow++, row++) {				
-			for(var indexCol = 2, col = 0; indexCol < numeroColunas; indexCol++, col++) {
-				nomeEntrada = "input" + indexRow + indexCol;				
-				if($("#"+nomeEntrada).val() == ""){
-					tableCalc[row][col] = 0;
+		for(var indexRow = 1; indexRow < nLines; indexRow++, row++) {				
+			for(var indexCol = 2, col = 0; indexCol < nCols; indexCol++, col++) {
+				inputName = "input" + indexRow + indexCol;				
+				if($("#"+inputName).val() == ""){
+					arraySimplex[row][col] = 0;
 				} else {
-					tableCalc[row][col] = parseFloat($("#"+nomeEntrada).val());
+					arraySimplex[row][col] = parseFloat($("#"+inputName).val());
 				}
 			}
 
@@ -163,56 +177,62 @@ function getValues(){
 	}
 	
 	mTable = false;
-	row = numeroLinhas - 1;
+	row = nLines - 1;
 	row--;
 	
 	// MENOR VALOR DA LINHA Z 
 	for(var col = 0; col < arrayColumn-1; col++) {	
-		minor = Math.min(tableCalc[row][col], minor);		
+		minor = Math.min(arraySimplex[row][col], minor);		
 	}
 	for(var col = 0; col < arrayColumn-1; col++) {
-		if(tableCalc[row][col] == minor) {
+		if(arraySimplex[row][col] == minor) {
 			column = col;
 			break;
 		}
 	}
 	// MONTA UM VETOR COM O MENOR VALOR DA LINHA Z
 	for(var row = 0; row < arrayRow; row++) {
-		arrayMinorValue[row] = tableCalc[row][column];
+		arrayMinorValue[row] = arraySimplex[row][column];
 	}
-	//alert("4 " + agoraVai);
+
 	// MONTAR VETOR DA COLUNA B
 	for(var rows = 0; rows < arrayRow-1; rows++) {		
-		arrayB[rows] = tableCalc[rows][arrayColumn];
+		arrayB[rows] = arraySimplex[rows][arrayColumn];
 	}	
-	//alert("5 " + agoraVai);
-	agoraVai = 0;
-	//simplexCalc();
+	
+	cGetValues = 0;
 }
 
 function simplexCalc() {
-	cont = 0;
+
+	// CONTROLE DE REPETIÇÃO
+	cRepeat = 0;
+
+	// CASO O USUARIO ENTROU COM UM NUMERO MAXIMO DE REPETIÇÃO ARMAZENA ESSE VALOR NA VARIAVEL nRepeat
 	if($("#nRepeat").val() != "") {
 		var nRepeat = parseInt($("#nRepeat").val());
 	}	
 	do{		
-		if(agoraVai == 0){			
+
+		// CONTROLE PARA PEGAR VALORES DA TABELA ANTERIOR
+		if(cGetValues == 0){			
 			minor = 9999;
-			agoraVai = 1;			
+			cGetValues = 1;			
 			getValues();
 		}
-	
+
 		minor = 9999;
-		var arrayDiv = [];
+		arrayDiv = [];
 		hLine = column+1;
 
-		// LINHA A SER TROCADA
+		// COLUNA B / COLUNA QUE IRÁ ENTRAR NA BASE
 		for(var index = 0; index < arrayMinorValue.length; index++) {
 			if(arrayMinorValue[index] != 0 && arrayB[index] != 0) {
 				arrayDiv[index] = arrayB[index] / arrayMinorValue[index];				
 			}
 		}		
-		
+
+		// LINHA EM QUE A COLUNA IRÁ ENTRAR
 		for(var index = 0; index < arrayDiv.length; index++) {
 			if(arrayDiv[index] < minor) {
 				minor = arrayDiv[index];
@@ -220,13 +240,14 @@ function simplexCalc() {
 			}
 		}
 
-		var Pivo = arrayMinorValue[column];
+		// SETA O VALOR DO PIVO
+		Pivo = arrayMinorValue[column];
 
 	
-
+		// CONSTRUÇÃO DAS TABELAS DE RESULTADO
 		if(check){
 		    var table = document.createElement("TABLE");
-		    table.setAttribute("id", "resultTable"+cont);
+		    table.setAttribute("id", "resultTable"+cRepeat);
 		    var thead = document.createElement("THEAD");
 		    var tbody = document.createElement("TBODY");
 		    table.appendChild(thead);
@@ -234,73 +255,80 @@ function simplexCalc() {
 		    table.className = "table";
 		    document.getElementById('result').appendChild(table);
 
-			montarTabela("resultTable"+cont);
+			mountTable("resultTable"+cRepeat);
 		}
 
-
+		// VERIFICA SE O CHECKBOX cbResult ESTÁ MARCADO CASO SIM CONSTRUIRÁ APENAS UMA TABELA
 		if(document.getElementById('cbResult').checked){
 			check = false;
-			idMatriz = 0;
+			idArray = 0;
 		} else {
-			idMatriz = cont;
+			idArray = cRepeat;
 		}
 
+		// HABILITADA A DIV DE RESULTADO
 		document.getElementById("result").style.display = "inline";
+		// DESABILITADA A DIV DE INICIAL
 		document.getElementById("initial").style.display = "none";
 
+		// PREENCHE A TABELA RESULTADO
 		var hCol = column+1;
-		if(cont != 0) {
-			var c = cont - 1;
-			for(var index = 0; index < numeroColunas; index++) {
+		if(cRepeat != 0) {
+			var c = cRepeat - 1;
+			for(var index = 0; index < nCols; index++) {
 				arrayBase[index] = $("#resultTable"+c+index+"1").text();
-			}
-			alert(arrayBase);
-			for(var index = 0; index < numeroColunas; index++) {
-				$("#resultTable"+cont+index+"1").text(arrayBase[index]);
+			}			
+			for(var index = 0; index < nCols; index++) {
+				$("#resultTable"+cRepeat+index+"1").text(arrayBase[index]);
 			}
 		}
 
-		$("#resultTable"+idMatriz+hCol+"1").text($("#resultTable"+idMatriz+hLine).text());		
+		$("#resultTable"+idArray+hCol+"1").text($("#resultTable"+idArray+hLine).text());		
 		
 		// Linha / Pivo 
 		for(var index = 0; index <= arrayColumn; index++) {
-			if(tableCalc[column][index] != 0) {			
-				tableCalc[column][index] = tableCalc[column][index] / Pivo;
+			if(arraySimplex[column][index] != 0) {			
+				arraySimplex[column][index] = arraySimplex[column][index] / Pivo;
 			}
 		}
 		
 		//ZERANDO A COLUNA
 		for(var index = 0; index < arrayMinorValue.length; index++) {
 			if(arrayMinorValue[index] != 0) {
-				for(var indexCol = 0; indexCol < tableCalc[0].length; indexCol++) {
+				for(var indexCol = 0; indexCol < arraySimplex[0].length; indexCol++) {
 					if(column != index) {
-						tableCalc[index][indexCol] = parseFloat((tableCalc[column][indexCol] * (-arrayMinorValue[index])) + tableCalc[index][indexCol]);						
+						arraySimplex[index][indexCol] = parseFloat((arraySimplex[column][indexCol] * (-arrayMinorValue[index])) + arraySimplex[index][indexCol]);						
 					}
 				}
 			}
 		}
-				
+		
 		if(!document.getElementById('cbResult').checked) {
-			idMatriz = cont;			
+			idArray = cRepeat;			
 		}
+
 		// MONTA A TABELA RESULTADO
-		for(var indexRow = 1, row = 0; indexRow < numeroLinhas; indexRow++, row++) {
-			for(var indexCol = 2, col = 0; indexCol < numeroColunas; indexCol++, col++) {	
-				$("#resultTable"+idMatriz+indexRow+indexCol).text(tableCalc[row][col]);
+		for(var indexRow = 1, row = 0; indexRow < nLines; indexRow++, row++) {
+			for(var indexCol = 2, col = 0; indexCol < nCols; indexCol++, col++) {	
+				$("#resultTable"+idArray+indexRow+indexCol).text(arraySimplex[row][col]);
 			}				
 		}
 		
-		teste = 0;
+		// VARIAVEL DE VERIFICAÇÃO DE PARADA.
+		cStop = 0;
 
-		for(var index = 0; index < numeroColunas-1; index++) {
-			if(tableCalc[numeroLinhas-2][index] < 0) {
-				//alert(tableCalc[numeroLinhas-2][index]);
-				teste++;
+		for(var index = 0; index < nCols-1; index++) {
+			if(arraySimplex[nLines-2][index] < 0) {				
+				cStop++;
 			}
 		}
-		if(nRepeat == cont)	{
+		
+		// VERIFICA SE A QUATIDADE DE REPETIÇÃO DEFINIDA PELO O USUARIO FOI ATINGIDA CASO SIM O PROGRAMA É FINALIZADO.
+		if(nRepeat == cRepeat)	{
 			break;
 		}
-		cont++;		
-	}while(teste != 0);
+
+		cRepeat++;		
+
+	}while(cStop != 0);
 }
